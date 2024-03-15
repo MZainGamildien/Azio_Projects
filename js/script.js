935429ea28af83192e7f4646f210d63470cff4d2
MZainGamildien <102467824+MZainGamildien@users.noreply.github.com>
2024-03-15 17:47:20 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,26 @@
+let navbar=document.querySelector('.header .navbar');
+let contactInfo = document.querySelector('.contact-info ');
+
+document.querySelector('#menu-btn').onclick=() =>{
+    navbar.classList.toggle('active');
+};
+document.querySelector('#info-btn').onclick=()=>{
+    contactInfo.classList.add('active');
+}
+document.querySelector('#close-contact-info').onclick=()=>{
+    contactInfo.classList.remove('active');
+}
+window.onscroll = () =>{
+    navbar.classList.remove('active');
+    contactInfo.classList.remove('active');
+
+}
+
+var swiper = new Swiper(".home-slider", {
+  loop:true,
+  grabCursor:true,
+    navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
